6a7f3b2fcbd31c3a25865a98d4782d78ed57a9da
Li Zhenhuan <zhenhuanlee@outlook.com>
2025-02-11 09:00:17 +0000

    remove file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 function App() {
+  const videoRef = useRef(null);
+  const [videos, setVideos] = useState([]);
+  const [videoSrc, setVideoSrc] = useState(null);
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+
+    console.log(files)
+
+    const file = files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setVideoSrc(url);
+    }
+  };
+
+  const handleKeyDown = useCallback((event) => {
+    console.log('clicked:', event.key);
+
+    if (videoRef.current) {
+      switch (event.key) {
+        case 'a':
+          videoRef.current.currentTime = Math.max(0, videoRef.current.currentTime - 10);
+          break;
+        case 'd':
+          videoRef.current.currentTime = Math.min(videoRef.current.duration, videoRef.current.currentTime + 10);
+          break;
+        case 'w':
+          const idx1 = Math.max(currentVideoIndex - 1, 0)
+          setCurrentVideoIndex(idx1);
+          videoRef.current.src = videos[idx1];
+          videoRef.current.play();
+          break;
+        case 's':
+          const idx2 = Math.min(currentVideoIndex + 1, videos.length - 1)
+          setCurrentVideoIndex(idx2);
+          videoRef.current.src = videos[idx2];
+          videoRef.current.play();
+          break;
+        case 'q':
+          break;
+        default:
+          break;
+      }
+    }
+  }, [currentVideoIndex, videos])
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, [handleKeyDown]);
+
+  const deleteFile = useCallback(async () => {
+    const [fileHandle] = await window.showOpenFilePicker();
+    if (!fileHandle) return
+    // await fileHandle.remove();
+    await fileHandle.remove();
+    console.log('File deleted successfully');
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: 'center'}}>
+      <button onClick={deleteFile}>删除文件</button>
+
+      <input type="file" webkitdirectory="true" onChange={handleFileChange} />
+
+      <video ref={videoRef} width="100%"  controls>
+        <source src={videoSrc} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 }
