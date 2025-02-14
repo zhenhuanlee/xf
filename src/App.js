8d0c1f0b9b631648754353ac78696f74d5dfb3bd
Li Zhenhuan <zhenhuanlee@outlook.com>
2025-02-14 10:36:01 +0000

    play

--- src/App.js
+++ src/App.js
@@ -5,23 +5,45 @@ function App() {
   const videoRef = useRef(null);
   const [videos, setVideos] = useState([]);
   const [videoSrc, setVideoSrc] = useState(null);
-  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [currentIdx, setCurrentIdx] = useState(0);
 
-  const handleFileChange = (event) => {
-    const files = event.target.files;
+  function isVideo (entry) {
+    return true
+  }
 
-    console.log(files)
-
-    const file = files[0];
-    if (file) {
-      const url = URL.createObjectURL(file);
-      setVideoSrc(url);
+  const handleFileChange = useCallback(async (event) => {
+    if (videos.length > 0) return
+    const directoryHandle = await window.showDirectoryPicker();
+    
+    const _videos = []
+    for await (const entry of directoryHandle.values()) {
+      if (isVideo(entry)) _videos.push(await entry.getFile());
     }
-  };
+
+    setVideos(_videos.sort(() => Math.random() - 0.5));
+  }, [videos.length]);
+
+  useEffect(() => {
+    if (videos.length <= 0) return
+
+    const videoUrl = URL.createObjectURL(videos[0]);
+    setVideoSrc(videoUrl);
+  }, [videos]);
+
+  useEffect(() => {
+    videoRef.current.load();
+  }, [currentIdx, videoSrc])
+
+  const handleSwitch = useCallback(async (idx) => {
+    console.log(idx, currentIdx)
+    if (idx === currentIdx) return
+
+    setCurrentIdx(idx)
+    setVideoSrc(URL.createObjectURL(videos[idx]));
+  }, [currentIdx, videos]);
 
   const handleKeyDown = useCallback((event) => {
     console.log('clicked:', event.key);
-
     if (videoRef.current) {
       switch (event.key) {
         case 'a':
@@ -30,45 +52,31 @@ function App() {
         case 'd':
           videoRef.current.currentTime = Math.min(videoRef.current.duration, videoRef.current.currentTime + 10);
           break;
+        case 'q':
+          break;
         case 'w':
-          const idx1 = Math.max(currentVideoIndex - 1, 0)
-          setCurrentVideoIndex(idx1);
-          videoRef.current.src = videos[idx1];
-          videoRef.current.play();
+          handleSwitch(Math.max(currentIdx - 1, 0));
           break;
         case 's':
-          const idx2 = Math.min(currentVideoIndex + 1, videos.length - 1)
-          setCurrentVideoIndex(idx2);
-          videoRef.current.src = videos[idx2];
-          videoRef.current.play();
-          break;
-        case 'q':
+          handleSwitch(Math.min(currentIdx + 1, videos.length - 1));
           break;
         default:
           break;
       }
     }
-  }, [currentVideoIndex, videos])
+  }, [currentIdx, handleSwitch, videos.length])
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
-  }, [handleKeyDown]);
 
-  const deleteFile = useCallback(async () => {
-    const [fileHandle] = await window.showOpenFilePicker();
-    if (!fileHandle) return
-    // await fileHandle.remove();
-    await fileHandle.remove();
-    console.log('File deleted successfully');
-  }, [])
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
 
   return (
     <div style={{textAlign: 'center'}}>
-      <button onClick={deleteFile}>删除文件</button>
-
-      <input type="file" webkitdirectory="true" onChange={handleFileChange} />
-
-      <video ref={videoRef} width="100%"  controls>
+      <video onClick={handleFileChange} ref={videoRef} className="fixed-video" autoPlay controls>
         <source src={videoSrc} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
